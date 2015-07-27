f7883b1a3d08af4d3a9e0de3fd547cba80ad1229
Buri Balázs <buribalazs@gmail.com>
2015-07-27 13:48:50 +0200

    add when functionality add expect functionality add verifynooutstandingrequests functionality add E2E tests add expect helper functions (expectGET etc.)

--- test/e2e/testMethods.js
+++ test/e2e/testMethods.js
@@ -9,6 +9,7 @@ describe('Test Http backend methods', function() {
     });
 
     afterEach(function() {
+        backend.verifyNoOutstandingExpectation();
         backend.clear();
     });
 
@@ -26,7 +27,7 @@ describe('Test Http backend methods', function() {
     it('Test whenGET with passThrough', function() {
         backend.whenGET("/result.json").passThrough();
         browser.get('/');
-        
+
 
         element(by.css('#buttonGET')).click();
 
@@ -38,7 +39,7 @@ describe('Test Http backend methods', function() {
         backend.whenGET(/result/).respond('raoul');
 
         browser.get('/');
-        
+
 
         element(by.css('#buttonGET')).click();
 
@@ -48,7 +49,7 @@ describe('Test Http backend methods', function() {
 
     it('Test whenPOST with string response', function() {
         backend.whenPOST(/result/).respond('raoul');
-        
+
         browser.get('/');
 
         element(by.css('#buttonPOST')).click();
@@ -68,6 +69,69 @@ describe('Test Http backend methods', function() {
 
         var result = element(by.binding('result'));
         expect(result.getText()).toEqual('postedData');
+    });
+
+    it('Test when (POST) with function as response', function() { //BB 01
+        backend.when('POST', /result/).respond(function(method, url, data) {
+            return [200, 'abuga'];
+        });
+
+        browser.get('/');
+
+        element(by.css('#buttonPOST')).click();
+
+        var result = element(by.binding('result'));
+        expect(result.getText()).toEqual('abuga');
+    });
+
+    it('Test expect function', function() { //BB 01
+        backend.expect('POST', /result/);
+        browser.get('/');
+        element(by.css('#buttonPOST')).click();
+    });
+
+    it('Test expect function a second time (properly flushed??)', function() { //BB 01
+        backend.expect('POST', /result/);
+        browser.get('/');
+        element(by.css('#buttonPOST')).click();
+
+    });
+
+    it('Test expectGET function', function() { //BB 01
+        backend.expectGET(/result/);
+        browser.get('/');
+        element(by.css('#buttonGET')).click();
+    });
+
+    it('Test expectPOST function', function() { //BB 01
+        backend.expectPOST(/result/);
+        browser.get('/');
+        element(by.css('#buttonPOST')).click();
+    });
+
+    it('Test expectPOST and expectGET functions', function() { //BB 01
+        backend.expectPOST(/result/);
+        backend.expectGET(/result/);
+        browser.get('/');
+        element(by.css('#buttonPOST')).click();
+        element(by.css('#buttonGET')).click();
+    });
+
+    it('Test combined when and expect functions', function() { //BB 01
+        backend.expectPOST(/result/);
+        backend.expectGET(/result/);
+        backend.whenPOST(/result/).respond('raoulPOST');
+        backend.whenGET(/result/).respond('raoulGET');
+        browser.get('/');
+
+        element(by.css('#buttonPOST')).click();
+        var result = element(by.binding('result'));
+        expect(result.getText()).toEqual('raoulPOST');
+
+        element(by.css('#buttonGET')).click();
+        result = element(by.binding('result'));
+        expect(result.getText()).toEqual('raoulGET');
+
     });
 
     it('Test whenPOST with string response', function() {
